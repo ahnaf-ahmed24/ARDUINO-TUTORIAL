fa784fd02a954f872d8bd7336bb0f88a193c13e6
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-12-03 19:15:36 +0600

    Class - 09

---
+++ Class - 09 ( Relay module with Arduino )/relay/relay.ino
@@ -0,0 +1,16 @@
+//YouTube|Tech at Home
+
+int relay=2;
+
+void setup() 
+{
+ pinMode(2,OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(relay,LOW);
+  delay(2000);
+  digitalWrite(relay,HIGH);
+  delay(2000);
+}
