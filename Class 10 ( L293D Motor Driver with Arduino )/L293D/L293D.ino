f113f785facd46052c1d385a94036a27b65e2ba8
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-12-05 19:07:39 +0600

    class 10

---
+++ Class 10 ( L293D Motor Driver with Arduino )/L293D/L293D.ino
@@ -0,0 +1,23 @@
+//YouTube|Tech at Home
+
+int a=8;
+int b=9;
+
+void setup() 
+{
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop()
+{
+  //Forward
+digitalWrite(a,HIGH);
+digitalWrite(b,LOW);
+delay(1000);
+
+//Backward
+digitalWrite(a,LOW);
+digitalWrite(b,HIGH);
+delay(1000);
+}
