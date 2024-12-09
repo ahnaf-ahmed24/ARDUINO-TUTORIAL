9b44f78d9e55684915b3e4974bd276267742a1d8
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-12-09 11:15:44 +0600

    Class - 11

---
+++ Class - 11 ( L298N motor driver with Arduino )/l298n/l298n.ino
@@ -0,0 +1,16 @@
+//YouTube|Tech at Home
+
+int in1=8;
+int in2=9;
+
+void setup()
+{
+ pinMode(8,OUTPUT);
+ pinMode(9,OUTPUT);
+}
+
+void loop() 
+{
+digitalWrite(in1,LOW);  
+digitalWrite(in2,HIGH);  
+}
